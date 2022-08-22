857d9583aa31a316d095c344b214b6514b70c932
elakia <vmelakia@gmail.com>
2022-08-22 18:48:10 +0530

    changed the port number to 8000

--- app.py
+++ app.py
@@ -70,7 +70,7 @@ if __name__ == '__main__':
     uvicorn.run(
         "app:app",
         host="0.0.0.0",
-        port=8800,
+        port=8000,
         reload=True,
         access_log=False
     )
